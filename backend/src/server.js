2f662206d8465454cfe8a9b6559083ec889059ca
dhanraj-aadi24 <Y226101002@dhsgsu.edu.in>
2026-03-29 23:39:46 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,15 @@
 import authRoutes from "./routes/auth.route.js"
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 dotenv.config();
 
 import messageRoutes from "./routes/message.routes.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const app=express();
@@ -17,6 +23,17 @@ const PORT=process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+//Ready for deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get(/^\/.*/,(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
+
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port: "+PORT);
