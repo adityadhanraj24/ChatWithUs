681b8677b0762a310dc2ad3f89c444093337aecd
dhanraj-aadi24 <Y226101002@dhsgsu.edu.in>
2026-04-11 00:35:35 +0530

    Link Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const PORT = env.PORT || 3000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: ["https://chat-with-us-jh33.vercel.app", "http://localhost:5173"], credentials: true }));
 
 // console.log(process.env.PORT);
 
