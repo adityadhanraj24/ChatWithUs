4b9abeb99b639a9a577c9df79bcbca8dd9c647ed
dhanraj-aadi24 <Y226101002@dhsgsu.edu.in>
2026-04-11 00:30:35 +0530

    Route Change2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://chatwithus-production.up.railway.app/api" : "/api",
+    baseURL: "https://chatwithus-production.up.railway.app/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-export default axiosInstance;
+export default axiosInstance;
