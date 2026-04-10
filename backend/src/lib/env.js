2ebe60912740cc8b2c4acd190af757c6f914524c
dhanraj-aadi24 <Y226101002@dhsgsu.edu.in>
2026-04-11 00:19:34 +0530

    Route Change

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 export const env = {
     PORT: process.env.PORT || 5000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_URL: process.env.CLIENT_URL || 'https://chatwithus-production.up.railway.app',
+    CLIENT_URL: process.env.CLIENT_URL || 'https://chat-with-us-jh33.vercel.app',
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import axiosInstance from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatwithus-production.up.railway.app" : "https://chatwithus-production.up.railway.app";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatwithus-production.up.railway.app" : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
